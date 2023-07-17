dc82c65b36ee08766aa23d7f5b4b944cdc3ab544
Juliana Oliveira <134332673+JuhOliveira1202@users.noreply.github.com>
2023-07-17 12:07:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    //Return Inverso Número
+    static void Main(string[] args)
+    {
+        int num, inverso;
+
+        Console.Write("Digite um número: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        inverso = (-1 * num);
+
+        Console.WriteLine("O inverso de " + num + " é: " + inverso);
+    }
+}
